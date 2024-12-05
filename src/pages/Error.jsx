818123b9292f531aa6e1861f68515174e4ca4ba8
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-05 16:29:14 +0300

    Finish Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,11 @@
+import { NavLink } from "react-router-dom"
 function Error() {
   return (
-    <div>Error</div>
+    <div className="flex flex-col items-center justify-center my-52 gap-6">
+      <h1 className="text-8xl">404 Not Found</h1>
+      <p>Your visited page not found. You may go home page.</p>
+      <NavLink to='/' className="flex justify-center items-center text-white w-64 py-3 mt-10 bg-[#EA4335] text-base rounded-[4px]">Back to home page</NavLink>
+    </div>
   )
 }
 export default Error

dc6496780afb861bfc4ad2dd8397e3de5f1de5cd
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-06 16:46:40 +0300

    Finish sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { NavLink } from "react-router-dom"
 function Home() {
   return (
     <div>
-      <div className="grid grid-cols-2">
-        <div id="sidebar" className="flex flex-col gap-3 w-96 border border-r-2 pl-32 pr-3 pt-8">
+      <div className="grid grid-cols-2 pb-36">
+        <div id="sidebar" className="flex flex-col gap-3 w-96 border-r-[1px] pl-32 pr-3 pt-10">
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Woman's Fashion<FaAngleRight/></NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Men's Fashion<FaAngleRight/></NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Electronics</NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Home & Lifestyle</NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Medicine</NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Sports & Outdoor</NavLink>
-
+          <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Baby's & Toys</NavLink>
+          <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Groceries & Pets</NavLink>
+          <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Health & Beauty</NavLink>
         </div>
         <div className=""></div>
       </div>

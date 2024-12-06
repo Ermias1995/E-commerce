aeaba68a48fdd4db9344da734ddd332f62d8e1c7
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-06 22:29:14 +0300

    Add dot buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,13 +17,22 @@ function Home() {
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Groceries & Pets</NavLink>
           <NavLink to="/contact" className="text-base active:font-bold flex justify-between hover:font-bold">Health & Beauty</NavLink>
         </div>
-        <div className="flex justify-center items-center gap-4 pl-16 py-10 mt-10 bg-black">
+        <div className="relative flex justify-center items-center gap-4 pl-16 py-10 mt-10 bg-black">
           <div className="flex flex-col ustify-center gap-4">
             <h1 className="text-textColor text-base flex items-center gap-4"><FaApple className="text-5xl"/>iPhone 14 Series</h1>
             <h1 className="text-textColor text-5xl w-72">Up to 10% off Voucher</h1>
             <NavLink to="/contact" className="text-textColor text-base underline flex items-center gap-2">Shop Now<FaArrowRight/></NavLink>
           </div>
-          <img src={iPhone} alt="iPhone" className=""/>
+            <div className="absolute items-end justify-center flex gap-3 mt-80">
+              <button className="w-3 h-3 bg-gray-400 hover:bg-[#EA4335] hover:border hover:border-white rounded-full"></button>
+              <button className="w-3 h-3 bg-gray-400 hover:bg-[#EA4335] hover:border hover:border-white rounded-full"></button>
+              <button className="w-3 h-3 bg-gray-400 hover:bg-[#EA4335] hover:border hover:border-white rounded-full"></button>
+              <button className="w-3 h-3 bg-gray-400 hover:bg-[#EA4335] hover:border hover:border-white rounded-full"></button>
+              <button className="w-3 h-3 bg-gray-400 hover:bg-[#EA4335] hover:border hover:border-white rounded-full"></button>
+            </div>
+          <div>
+            <img src={iPhone} alt="iPhone" className=""/>
+          </div>
         </div>
       </div>
     </div>

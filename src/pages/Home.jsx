5a2bb8a7659b31ec29290a0be1db49a9a02a5127
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-08 09:39:18 +0300

    make products horizantally scrollabel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,7 @@ function Home() {
             </div>
           <div>
             {/* If I get time Image sliding has to be done */}
-            <img src={iPhone} alt="iPhone" className=""/>
+            <img src={iPhone} alt="iPhone" className="object-contain w-full h-full"/>
           </div>
         </div>
       </div>
@@ -98,18 +98,22 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="flex gap-8">
+        <div className="relative">
+          <button onClick={() => document.getElementById('product-list').scrollBy({ left: -300, behavior: 'smooth' })} className="absolute left-0 top-1/3 w-12 h-12 z-10 transform -translate-y-1/2 bg-gray-300 p-2 rounded-full">{"<"}</button>
+          <div id="product-list" className="flex gap-8 overflow-x-scroll scrollbar-hide">
             {items.map((product) => (
-                <div key={product.id} className="flex flex-col gap-10 w-[270px] h-[350px] mt-32">
-                  <div id='image section' className="bg-[#808080] border w-48 h-44 rounded relative flex items-center justify-center">
-                    <img src={product.image} alt={product.title}/>
+                <div key={product.id} className="flex flex-col gap-10 w-[270px] h-[350px]">
+                  <div id='image section' className="bg-textColor py-4 border w-48 h-44 rounded relative flex items-center justify-center">
+                    <img src={product.image} alt={product.title} className="object-contain w-full h-full"/>
                   </div>
                   <div className="flex flex-col gap-2">
                     <h2 className="text-base">{product.title.length > 20 ? `${product.title.slice(0, 20)}...` : product.title}</h2>
-                    <p className="text-secondary text-base">${product.price}<span className="text-[#808080] line-through ml-1">${product.price + 20}</span></p>
+                    <p className="text-secondary text-base">${product.price.toFixed(2)}<span className="text-[#808080] line-through ml-1">${(product.price + 20).toFixed(2)}</span></p>
                   </div>
                 </div>
             ))}
+          </div>
+          <button onClick={() => document.getElementById('product-list').scrollBy({ left: 300, behavior: 'smooth' })} className="absolute right-0 top-1/3 w-12 h-12 transform -translate-y-1/2 bg-gray-300 p-2 rounded-full">{">"}</button>
         </div>
       </div>
     </div>

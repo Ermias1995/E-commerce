294ac190d078876451f1e9ec78f830ae64e4dc3f
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-08 23:56:32 +0300

    Finish second section of Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 import afgirls from "../assets/afgirls.png";
+import { FaHome,FaShoppingBag } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa6";
+import { GiGymBag } from "react-icons/gi";
 const Contact = () =>{
     return(
-        <div className="mt-36">
+        <div className="flex flex-col mt-36 gap-36">
             <div id="First section" className="flex items-center justify-center pl-[135px] gap-10">
                 <div className="flex flex-col gap-16">
                     <h1 className="text-[54px] font-semibold">Our Story</h1>
@@ -16,6 +19,28 @@ const Contact = () =>{
                 </div>
                 <div className="flex items-center justtify-center">
                     <img src={afgirls} alt="afgirls"/>
+                </div>
+            </div>
+            <div id="2nd section" className="flex items-center justify-center px-[135px] gap-4">
+                <div className="flex flex-col items-center gap-2 border rounded bg-textColor hover:text-textColor hover:bg-secondary py-5 px-10">
+                    <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaHome/></div>
+                    <h1 className="text-3xl font-bold">10.5k</h1>
+                    <p className="text-base">Sallers active our site</p>
+                </div>
+                <div className="flex flex-col items-center gap-2 border rounded bg-textColor hover:text-textColor hover:bg-secondary py-5 px-10">
+                    <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaHome/></div>
+                    <h1 className="text-3xl font-bold">33k</h1>
+                    <p className="text-base">Monthly Product Sale</p>
+                </div>
+                <div className="flex flex-col items-center gap-2 border rounded bg-textColor hover:text-textColor hover:bg-secondary py-5 px-10">
+                    <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaHome/></div>
+                    <h1 className="text-3xl font-bold">45.5k</h1>
+                    <p className="text-base">Customer active in our site</p>
+                </div>
+                <div className="flex flex-col items-center gap-2 border rounded bg-textColor hover:text-textColor hover:bg-secondary py-5 px-10">
+                    <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaHome/></div>
+                    <h1 className="text-3xl font-bold">25k</h1>
+                    <p className="text-base">Annual gross sale in our site</p>
                 </div>
             </div>
         </div>

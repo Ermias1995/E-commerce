6713f0c63450287447788936c3de43285e5e8eb0
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-08 22:24:08 +0300

    Finish UI part of Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { FaAngleRight,FaApple,FaArrowRight,FaTruckFast } from "react-icons/fa6";
+import { FaAngleRight,FaApple,FaArrowRight,FaTruckFast,FaHeadset } from "react-icons/fa6";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import iPhone from "../assets/iPhone.png";
 import Tape from "../assets/Tape.png";
@@ -245,11 +246,21 @@ function Home() {
         </div>
 
         {/* Last section */}
-        <div className="flex justify-around items-center">
+        <div className="flex justify-around items-center my-20">
           <div className="flex flex-col items-center gap-2">
             <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaTruckFast/></div>
-            <h1 className="text-xl">FREE AND FAST DELIVERY</h1>
+            <h1 className="text-xl font-semibold">FREE AND FAST DELIVERY</h1>
             <p className="text-sm">Free delivery for all orders over $140</p>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><FaHeadset/></div>
+            <h1 className="text-xl font-semibold">24/7 CUSTOMER SERVICE</h1>
+            <p className="text-sm">Friendly 24/7 customer support</p>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex items-center justify-center text-4xl bg-black text-white border-8 rounded-full p-2 w-20 h-20"><AiOutlineSafetyCertificate/></div>
+            <h1 className="text-xl font-semibold">MONEY BACK GUARANTEE</h1>
+            <p className="text-sm">We reurn money within 30 days</p>
           </div>
         </div>
       </div>

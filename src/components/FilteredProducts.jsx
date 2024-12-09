268ae56dc37b202f10ac50d4e5be463a5331912a
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-09 20:59:38 +0300

    Update productList pages and SelectedProducts

--- src/components/FilteredProducts.jsx
+++ src/components/FilteredProducts.jsx
@@ -25,39 +25,23 @@ const FilteredProducts = () => {
 
     return (
         <div className="flex gap-8 overflow-x-scroll">
-            {/* {filteredProducts.map((product) => (
-                <div key={product.id} className="flex flex-col gap-10 w-[270px] h-[350px]">
-                    <div className="bg-textColor py-4 border w-48 h-44 rounded relative flex items-center justify-center">
+            {filteredProducts.map((product) => (
+                <div key={product.id} className="group flex flex-col hover:cursor-pointer gap-10 w-[270px] h-[350px]">
+                    <div id='image section' className=" bg-textColor py-4 border w-48 h-44 rounded relative flex flex-col items-center justify-center">
+                    <NavLink to={`/product/${product.id}`} className="group flex flex-col hover:cursor-pointer object-contain w-full h-full">
                         <img src={product.image} alt={product.title} className="object-contain w-full h-full"/>
-                        <button className="absolute flex items-center justify-center top-2 right-2 p-1 w-8 h-8 text-base rounded-full bg-white hover:bg-secondary hover:text-textColor"><FaRegHeart/></button>
+                    </NavLink>
+                        <button className="bg-black px-5 py-2 w-full hidden text-textColor group-hover:flex items-center justify-center rounded-b" onClick={() => handleAddToCart(product)}>Add to Cart</button>
+                        <button className="absolute flex items-center justify-center top-2 right-2 p-1 w-8 h-8 text-base rounded-full bg-white hover:bg-secondary hover:text-textColor" onClick={() => handleAddToWishlist(product)}><FaRegHeart/></button>
                         <button className="absolute flex items-center justify-center top-10 right-2 p-1 w-8 h-8 text-base rounded-full bg-white hover:bg-secondary hover:text-textColor"><MdOutlineRemoveRedEye/></button>
+                        <p className="absolute flex items-center justify-center top-2 left-2 py-1 px-4 w-8 h-8 text-xs rounded bg-secondary text-textColor">-30%</p>
                     </div>
                     <div className="flex flex-col gap-2">
                         <h2 className="text-base">{product.title.length > 20 ? `${product.title.slice(0, 20)}...` : product.title}</h2>
-                        <p className="text-secondary text-base">
-                            ${product.price.toFixed(2)}
-                            <span className="text-[#808080] line-through ml-1">${(product.price + 20).toFixed(2)}</span>
-                        </p>
+                        <p className="text-secondary text-base">${product.price.toFixed(2)}<span className="text-[#808080] line-through ml-1">${(product.price + 20).toFixed(2)}</span></p>
                     </div>
                 </div>
-            ))} */}
-            {filteredProducts.map((product) => (
-                    <div key={product.id} className="group flex flex-col hover:cursor-pointer gap-10 w-[270px] h-[350px]">
-                            <div id='image section' className=" bg-textColor py-4 border w-48 h-44 rounded relative flex flex-col items-center justify-center">
-                            <NavLink to={`/product/${product.id}`} className="group flex flex-col hover:cursor-pointer object-contain w-full h-full">
-                                <img src={product.image} alt={product.title} className="object-contain w-full h-full"/>
-                            </NavLink>
-                                <button className="bg-black px-5 py-2 w-full hidden text-textColor group-hover:flex items-center justify-center rounded-b" onClick={() => handleAddToCart(product)}>Add to Cart</button>
-                                <button className="absolute flex items-center justify-center top-2 right-2 p-1 w-8 h-8 text-base rounded-full bg-white hover:bg-secondary hover:text-textColor" onClick={() => handleAddToWishlist(product)}><FaRegHeart/></button>
-                                <button className="absolute flex items-center justify-center top-10 right-2 p-1 w-8 h-8 text-base rounded-full bg-white hover:bg-secondary hover:text-textColor"><MdOutlineRemoveRedEye/></button>
-                                <p className="absolute flex items-center justify-center top-2 left-2 py-1 px-4 w-8 h-8 text-xs rounded bg-secondary text-textColor">-30%</p>
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-base">{product.title.length > 20 ? `${product.title.slice(0, 20)}...` : product.title}</h2>
-                                <p className="text-secondary text-base">${product.price.toFixed(2)}<span className="text-[#808080] line-through ml-1">${(product.price + 20).toFixed(2)}</span></p>
-                            </div>
-                        </div>
-                ))}
+            ))}
         </div>
     );
 };

5f9c44a7802260c99a96967bbbaef858fe8aa7d4
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-12-09 03:01:55 +0300

    Finish  Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,8 @@ import { IoCall } from "react-icons/io5";
 
 const Contact = () =>{
     return(
-        <div className="flex gap-5">
-            <div className="flex flex-col gap-1 py-8 px-4 rounded shadow-md">
+        <div className="flex gap-5 px-[135px] my-28 py-10">
+            <div className="flex flex-col gap-1 py-12 px-4 rounded shadow-md">
                 <h1 className="text-base flex items-center gap-3"><IoCall className="bg-secondary rounded-full text-white p-2 text-3xl"/>Call To Us</h1>
                 <p className="text-sm">We are available 24/7, 7 days a week.</p>
                 <p className="text-sm">Phone: +8801611112222</p>
@@ -13,7 +13,15 @@ const Contact = () =>{
                 <p className="text-sm">Emails: customer@exclusive.com</p>
                 <p className="text-sm">Emails: support@exclusive.com</p>
             </div>
-            <div className=""></div>
+            <div className="flex flex-col gap-6 rounded shadow-md p-4 py-12">
+                <div className="flex gap-8">
+                    <input type="text" placeholder="Your Name" className="bg-textColor p-2"/>
+                    <input type="email" placeholder="Your Email" className="bg-textColor p-2"/>
+                    <input type="phone" placeholder="Your Phone" className="bg-textColor p-2"/>
+                </div>
+                <textarea className="h-[207px] w-[737px] bg-textColor p-4" placeholder="Your Message"></textarea>
+                <button className="bg-secondary py-2 px-6 w-56 text-textColor rounded flex items-center justify-center">Send Message</button>
+            </div>
         </div>
     );
 }
